2ab7e33f1348880539d30b6dbe91cd06e02157f5
Gill Erick <erick@sendyit.com>
2022-08-11 16:20:11 +0300

    Create GET operation

--- src/db/redis.go
+++ src/db/redis.go
@@ -5,3 +5,11 @@ type Store map[string]string
 
 //ReverseStore is the reverse of Store and maps value to key(s) e.g. {"foo" : ["a", "b"]}
 type ReverseStore map[string][]string
+
+func get(key string, store Store) (string, error) {
+	if value, ok := store[key]; ok {
+		return value, nil
+	} else {
+		return "NULL", nil
+	}
+}
