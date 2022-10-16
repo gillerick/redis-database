5ba4cc8a43b7ba5615a26b690df690fe5260b555
Gill Erick <ogayogill95@gmail.com>
2022-10-16 18:08:48 +0300

    Update commands: Add INCR and DECR

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ import (
 
 const prompt = ">> "
 const commands =
-	`GET [key]: Prints value for a given key and NULL if value not in database
-	 SET [key] [value]: Sets database key to the specified value. Returns 'OK' if operation successful
-     DEL [key]: Deletes a key-value pair from the database
-     COUNT [value]: Prints the number of keys that have the given value assigned to them. Prints 0 if value not assigned anywhere
-	 EXISTS [key]: Prints 1 if value with key exists, otherwise prints 0`
+	`GET key: Prints value for a given key and NULL if value not in database
+	 SET key value: Sets database key to the specified value. Returns 'OK' if operation successful
+     DEL key: Deletes a key-value pair from the database
+     COUNT value: Prints the number of keys that have the given value assigned to them. Prints 0 if value not assigned anywhere
+	 EXISTS key: Prints 1 if value with key exists, otherwise prints 0
+     INCR key [increment]: Increments a value by increment (if provided), otherwise by 1
+     DECR key [decrement]: Decrements a value by decrement (if provided), otherwise by 1
+	 `
 
 func main() {
 	store := make(db.Store)
